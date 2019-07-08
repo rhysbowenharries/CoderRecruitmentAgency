d0e77a804ba941848eab289f5f799c5dbb2278cc
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-07-08 13:38:23 +0100

    improved css for the list profiles

--- src/containers/ProfileListContainer.js
+++ src/containers/ProfileListContainer.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import ProfileList from "../components/ProfileList"
+import "./profileContainer.css"
 
 class ProfileListContainer extends Component{
 
@@ -26,7 +27,18 @@ class ProfileListContainer extends Component{
     render(){
         return(
             <Fragment>
-                    <ProfileList profiles= {this.state.profiles}/>
+                <div className = "search-options">
+                <select className = "select-list">
+                    <option> Select job tiles ... </option>
+                    <option value = "Software Engineer"> Software Engineer </option>
+                    <option value = "Engineer Management"> Engineer Management </option>
+                    <option value = "UX"> Software Engineer </option>
+                    <option value = "Product Manager"> Product Manager </option>
+                    <option value = "Front-end Developer"> Front-end Developer </option>
+                    <option value = "Mobile App Engineer"> Mobile App Engineer </option>
+                </select>
+                </div>
+                <ProfileList profiles= {this.state.profiles}/>
             </Fragment>
         )
     }
