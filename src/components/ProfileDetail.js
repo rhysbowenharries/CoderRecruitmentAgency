57b95b8e2da28b500175723a5618f5922b1c2441
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-07-08 10:04:09 +0100

    stuff

--- src/components/ProfileDetail.js
+++ src/components/ProfileDetail.js
@@ -8,14 +8,16 @@ const ProfileDetail = (props) => {
         <Fragment>
 
         <div className ="list-profile">
-        <div>
+        <div className = "margin-right">
         <img src={logo} alt="Italian Trulli" className="profile"></img>
         </div>
         <div>
             <li>
                 <h2><strong>{props.first_name} {props.last_name}</strong> - Software Engineer</h2>
-                <h4 className ="langauge">{props.languages}</h4>
                 <h4> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h4>
+                <div className ="langauge">
+                <h4>{props.languages}</h4>
+                </div>
             </li>
             <button className="view-profile-button">View Profile</button>
         </div>
