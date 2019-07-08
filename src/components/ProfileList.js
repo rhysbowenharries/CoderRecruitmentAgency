04ed89ae64799338d7262647728890c81df6f82d
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-07-08 13:07:26 +0100

    upgrade css to profile list

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { Fragment} from 'react'
 import ProfileDetail from './ProfileDetail'
 import "./ProfileDetail.css"
+import "./ProfileList.css"
 
 const ProfileList = (props) => {
 
-    const options = props.profiles.map( (profile, index) => {
 
+
+    const options = props.profiles.map( (profile, index) => {
         return <ProfileDetail
-        key= {index}
-        first_name = {profile.first_name}
-        last_name = {profile.last_name}
-        languages = {profile.languages}
+            key= {index}
+            first_name = {profile.first_name}
+            last_name = {profile.last_name}
+            languages = {profile.languages}
         />
     })
 
+
     return(
-        <div >
-        <ul >{options}</ul>
-        </div>
+        <Fragment>
+            <div className ="profileList">
+                <ul className ="ul">{options}</ul>
+            </div>
+        </Fragment>
     )
 }
 
