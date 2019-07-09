54af32a0cc5edfa0622ec5104a690b39a2351562
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-07-09 16:30:25 +0100

    added in the avatar

--- src/components/ProfileDetail.js
+++ src/components/ProfileDetail.js
@@ -14,6 +14,7 @@ class ProfileDetail extends Component{
         this.state = {
             toProfileList: false
         }
+        this.extraProfileInfoContainer = React.createRef();
         this.handleButtonSelection = this.handleButtonSelection.bind(this)
     }
 
@@ -21,50 +22,55 @@ class ProfileDetail extends Component{
         this.setState({toProfileList: true})
     }
 
-    render(){
-        // if(this.state.toProfileList){
-        //
-        //         return<Redirect to={
-        //             path:`/start_hire/${this.props.id}`,
-        //             state: {this.props}
-        //             }/>
-        // }else{
-            return (
-                <Fragment>
-                    {console.log(this.props)}
-                    
-                    <div className ="list-profile">
-
-
-                        <div className = "margin-right">
-                            <img src={profileimage} alt="Italian Trulli" className="profile"></img>
-                        </div>
-
-                        <div>
-                            <li>
-                                <div className = "profile-header-info">
-                                    <a><h2><strong>{this.props.first_name} {this.props.last_name}</strong> - Software Engineer</h2></a>
-                                    <button onClick = {this.handleButtonSelection}  className="view-profile-button" value={this.props.id}>View Profile</button>
-                                </div>
-                                <div className = "profile-sub-info">
-                                    <p> Asking Salary:£{this.props.salary} </p>
-                                    <p> Location Base: {this.props.location} </p>
-                                    <p> Role: {this.props.role} </p>
-                                    <p>Speciality: {this.props.jobTitle}</p>
-                                </div>
-                                <h4> {this.props.profileDescription}</h4>
-                                    <div className ="langauge">
-                                        <p>{this.props.languages}</p>
-                                    </div>
-                                </li>
-                            </div>
-
-                        </div>
-                    </Fragment>
-                )
-            // }
+    handleToggleProfile = (event) => {
+        const extraProfileInfoContainer = this.extraProfileInfoContainer.current;
+        if(extraProfileInfoContainer.classList.contains('hidden')){
+            extraProfileInfoContainer.classList.remove('hidden')
+        } else {
+            extraProfileInfoContainer.classList.add('hidden')
         }
     }
 
 
-    export default ProfileDetail
+    render(){
+        return (
+            <Fragment>
+                {console.log(this.props)}
+
+                <div className ="list-profile">
+
+
+                    <div className = "margin-right">
+                        <img src={profileimage} alt="Italian Trulli" className="profile"></img>
+                    </div>
+
+                    <div>
+                        <li>
+                            <div className = "profile-header-info">
+                                <a><h2><strong>{this.props.first_name} {this.props.last_name}</strong> - Software Engineer</h2></a>
+                                <button onClick = {this.handleToggleProfile}  className="view-profile-button" value={this.props.id}>View Profile</button>
+                            </div>
+
+                            <h4> {this.props.profileDescription}</h4>
+                            <div className ="langauge">
+                                <p>{this.props.languages}</p>
+                            </div>
+
+                            <div ref={this.extraProfileInfoContainer} className = "hidden profile-sub-info">
+                                <p> Asking Salary:£{this.props.salary} </p>
+                                <p> Location Base: {this.props.location} </p>
+                                <p> Role: {this.props.role} </p>
+                                <p>Speciality: {this.props.jobTitle}</p>
+                            </div>
+                        </li>
+                    </div>
+
+                </div>
+            </Fragment>
+        )
+        // }
+    }
+}
+
+
+export default ProfileDetail
